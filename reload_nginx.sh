9e495e5bbf6f721a8a774cc2f999ca3f3b1d5699
joeydash <joydassudipta@gmail.com>
2018-12-30 01:49:59 +0530

    feat sh changed

--- reload_nginx.sh
+++ reload_nginx.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-cd ~/Accomox
 git pull
-sudo cp -rf ~/Accomox/default /etc/nginx/sites-available/default
+sudo cp -rf default /etc/nginx/sites-available/default
 sudo service nginx restart
