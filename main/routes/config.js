19bb8d63fd7817bb55ecba2f5217e679c0f475a6
joeydash <joydassudipta@gmail.com>
2019-04-30 07:30:03 +0530

    edited

--- main/routes/config.js
+++ main/routes/config.js
@@ -1,7 +1,8 @@
 let graphql_host = process.env.GRAPHQL_ENGINE_DATABASE_HOST || "localhost";
 
 let config = {
-    google_clientId: "486280821758-hmhh5k16bq1mh3h5ttht28j4bb3k40dl.apps.googleusercontent.com",
+    google_clientId: "108714462312-35l9judrs9fh2oeoj9hj5eeihbglhg3p.apps.googleusercontent.com",
+    // V_2MBOl4vBE6xvm0XXIg-s34
     cloudinary_api_id: {
         cloud_name: 'joeydash',
         api_key: '571664355428984',
